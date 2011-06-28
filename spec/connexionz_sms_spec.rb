0623281b4ab8d244cf2283c60fd1eca9f69ebd81
Ryan Resella <ryan@codeforamerica.org>
2011-06-27 17:32:07 -0700

    Updated specs

--- spec/connexionz_sms_spec.rb
+++ spec/connexionz_sms_spec.rb
@@ -1,20 +1,35 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe 'Connexionz SMS Application' do
-  it "Should return the home page" do
-    get '/'
-    last_response.should be_ok
+  
+  describe "the home page" do
+    it "Should return the home page" do
+      get '/'
+      last_response.should be_ok
+    end
+  end
+
+  describe "passing a parameter" do
+    it "returns a parameter" do
+      get '/hello/ryan'
+      last_response.should be_ok
+      last_response.body.should == 'Hello ryan!'
+    end
   end
   
-  it "returns a parameter" do
-    get '/hello/ryan'
-    last_response.should be_ok
-    last_response.body.should == 'Hello ryan!'
+  describe "route_et" do
+    it "should return no bus stop found" do
+      get '/route_et/10000'
+      last_response.should be_ok
+      last_response.body.should == 'No bus stop found'
+    end
+    
+    it "should return no arrivals for scope" do
+      get '/route_et/15414'
+      last_response.should be_ok
+      last_response.body.should == "No arrivals for next 30 minutes"
+    end
   end
   
-  it "should return no bus stop found" do
-    get '/route_et/10000'
-    last_response.should be_ok
-    last_response.body.should == 'No bus stop found'
-  end
+  
 end
