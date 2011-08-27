caa78a00978d318061fddb439ce9c0b85422243e
Ryan Resella <ryan@codeforamerica.org>
2011-08-27 16:50:21 -0700

    Clean up white space

--- connexionz_sms.rb
+++ connexionz_sms.rb
@@ -3,54 +3,53 @@
  require 'connexionz'
  require 'smsified'
  require 'json'
- 
+
  set :sms_user, ENV['SMS_USER'] || 'foo'
  set :password, ENV['PASSWORD'] || 'secret'
  set :sender_phone, ENV['SMS_PHONE'] || '555-555-1212'
- 
- 
+
  get '/' do
    "Real time Bus Notification System for SMS.  Text the bus stop number to 585-326-0523 (example 10279)"
  end
- 
+
  get '/hello/:name' do
    # matches "GET /hello/foo" and "GET /hello/bar"
    # params[:name] is 'foo' or 'bar'
    "Hello #{params[:name]}!"
  end
- 
+
  get '/route_et/:name' do
    #matches "GET /route_et/19812"
    @client = Connexionz::Client.new({:endpoint => "http://12.233.207.166/"})
    @platform_info = @client.route_position_et({:platformno => "#{params[:name]}"})
-   
+
    if @platform_info.route_position_et.platform.nil?
      sms_message = "No bus stop found"
    else
      name = @platform_info.route_position_et.platform.name
      arrival_scope = @platform_info.route_position_et.content.max_arrival_scope
      sms_message = ""
-   
+
      if @platform_info.route_position_et.platform.route.nil?
        sms_message = "No arrivals for next #{arrival_scope} minutes"
      elsif @platform_info.route_position_et.platform.route.class == Array
        @platforms = @platform_info.route_position_et.platform.route
-     
+
        @platforms.each do |platform|
-         sms_message += "Route #{platform.route_no} -Destination #{platform.destination.name} -ETA #{platform.destination.trip.eta } minutes"  
-       end 
+         sms_message += "Route #{platform.route_no} -Destination #{platform.destination.name} -ETA #{platform.destination.trip.eta } minutes"
+       end
      else
        route_no = @platform_info.route_position_et.platform.route.route_no
        destination = @platform_info.route_position_et.platform.route.destination.name
        eta = @platform_info.route_position_et.platform.route.destination.trip.eta
-       sms_message = "Route #{route_no} " + "-Destination #{destination} " + "-ETA #{eta} minutes" 
+       sms_message = "Route #{route_no} " + "-Destination #{destination} " + "-ETA #{eta} minutes"
      end
    end
 
    sms_message
  end
- 
- 
+
+
 post '/incoming' do
 
    response = request.env["rack.input"].read
@@ -64,7 +63,7 @@ post '/incoming' do
 
    puts text
    puts message
-   
+
    @client = Connexionz::Client.new({:endpoint => "http://12.233.207.166/"})
    @platform_info = @client.route_position_et({:platformno => "#{message}"})
    if @platform_info.route_position_et.platform.nil?
@@ -80,13 +79,13 @@ post '/incoming' do
         @platforms = @platform_info.route_position_et.platform.route
 
         @platforms.each do |platform|
-          sms_message += "Route #{platform.route_no} -Destination #{platform.destination.name} -ETA #{platform.destination.trip.eta } minutes"  
-        end 
+          sms_message += "Route #{platform.route_no} -Destination #{platform.destination.name} -ETA #{platform.destination.trip.eta } minutes"
+        end
       else
         route_no = @platform_info.route_position_et.platform.route.route_no
         destination = @platform_info.route_position_et.platform.route.destination.name
         eta = @platform_info.route_position_et.platform.route.destination.trip.eta
-        sms_message = "Route #{route_no} " + "-Destination #{destination} " + "-ETA #{eta} minutes" 
+        sms_message = "Route #{route_no} " + "-Destination #{destination} " + "-ETA #{eta} minutes"
       end
    end
 
--- spec/connexionz_sms_spec.rb
+++ spec/connexionz_sms_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe 'Connexionz SMS Application' do
-  
+
   describe "the home page" do
     it "Should return the home page" do
       get '/'
@@ -16,20 +16,19 @@ describe 'Connexionz SMS Application' do
       last_response.body.should == 'Hello ryan!'
     end
   end
-  
+
   describe "route_et" do
     it "should return no bus stop found" do
       get '/route_et/10000'
       last_response.should be_ok
       last_response.body.should == 'No bus stop found'
     end
-    
+
     it "should return no arrivals for scope" do
       get '/route_et/15414'
       last_response.should be_ok
       last_response.body.should == "No arrivals for next 30 minutes"
     end
   end
-  
-  
+
 end
