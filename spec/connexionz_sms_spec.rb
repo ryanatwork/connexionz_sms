90fbc62859c8b1ebb298e12761c97388e53f7eeb
Ryan Resella <ryan@codeforamerica.org>
2011-08-27 19:23:36 -0700

    Added additional specs

--- spec/connexionz_sms_spec.rb
+++ spec/connexionz_sms_spec.rb
@@ -19,15 +19,26 @@ describe 'Connexionz SMS Application' do
 
   describe "route_et" do
     it "should return no bus stop found" do
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?contenttype=SQLXML&Name=RoutePositionET.xml&platformno=10000").
+        to_return(:status => 200, :body => fixture("no_platform.xml"))
       get '/route_et/10000'
       last_response.should be_ok
       last_response.body.should == 'No bus stop found'
     end
 
     it "should return no arrivals for scope" do
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?contenttype=SQLXML&Name=RoutePositionET.xml&platformno=15414").
+        to_return(:status => 200, :body => fixture("no_arrivals.xml"))
       get '/route_et/15414'
       last_response.should be_ok
       last_response.body.should == "No arrivals for next 30 minutes"
+    end
+
+    it "should return the time for the next arrival" do
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?contenttype=SQLXML&Name=RoutePositionET.xml&platformno=10246").
+        to_return(:status => 200, :body => fixture("route_et.xml"))
+      get '/route_et/10246'
+      last_response.body.should == "Route 1-Destination Castaic-ETA 24 minutes Route 4-Destination LARC-ETA 19 minutes Route 6-Destination Shadow Pines-ETA 17 minutes Route 14-Destination Plum Cyn-ETA 11 minutes"
     end
   end
 
