df5c0ca436301c1177e08c86f1a818b579a603d9
Ryan Resella <ryan@codeforamerica.org>
2011-08-27 18:59:54 -0700

    Added def for fixture

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,19 @@ set :environment, :test
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
+
+  def fixture_path
+    File.expand_path('../fixtures', __FILE__)
+  end
+
+  def fixture(file)
+    File.new(fixture_path + '/' + file)
+  end
+
 end
 
 def app
   Sinatra::Application
 end
+
+
