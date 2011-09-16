caf383d1eea8acb134f424278993e4c312bb52f6
Ryan Resella <ryan@codeforamerica.org>
2011-09-15 18:43:24 -0700

    Updated specs for SMS section

--- spec/connexionz_sms_spec.rb
+++ spec/connexionz_sms_spec.rb
@@ -26,6 +26,15 @@ describe 'Connexionz SMS Application' do
       last_response.body.should == "No arrivals for next 30 minutes"
     end
 
+    it "should return the time for the one arrival" do
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?contenttype=SQLXML&Name=RoutePositionET.xml&platformno=10656").
+        to_return(:status => 200, :body => fixture("one_arrival.xml"))
+      get '/route_et/10656'
+      last_response.should be_ok
+      last_response.body.should =="Route 2 -Destination Val Verde -ETA 20 minutes"
+    end
+
+
     it "should return the time for the next arrival" do
       stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?contenttype=SQLXML&Name=RoutePositionET.xml&platformno=10246").
         to_return(:status => 200, :body => fixture("route_et.xml"))
@@ -34,4 +43,101 @@ describe 'Connexionz SMS Application' do
     end
   end
 
+  describe "incoming" do
+    it "should return no bus stop found message" do
+      json = '{
+                "inboundSMSMessageNotification": {
+                  "inboundSMSMessage": {
+                    "dateTime": "2011-05-11T18:05:54.546Z",
+                    "destinationAddress": "16575550100",
+                    "message": "10000",
+                    "messageId": "ef795d3dac56a62fef3ff1852b0c123a",
+                    "senderAddress": "14075550100"
+                  }
+                }
+              }'
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?Name=RoutePositionET.xml&contenttype=SQLXML&platformno=10000").
+        to_return(:status => 200, :body => fixture("no_platform.xml"))
+
+      stub_request(:post, "https://foo:secret@api.smsified.com/v1/smsmessaging/outbound/555-555-1212/requests").
+         with(:body => {"address"=>"14075550100", "message"=>"No bus stop found"}).
+         to_return(:status => 200, :body => "", :headers => {})
+
+      post :incoming, json
+      last_response.should be_ok
+    end
+
+    it "should return no arrivals for next 30 minutes" do
+      json = '{
+                "inboundSMSMessageNotification": {
+                  "inboundSMSMessage": {
+                    "dateTime": "2011-05-11T18:05:54.546Z",
+                    "destinationAddress": "16575550100",
+                    "message": "15414",
+                    "messageId": "ef795d3dac56a62fef3ff1852b0c123a",
+                    "senderAddress": "14075550100"
+                  }
+                }
+              }'
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?Name=RoutePositionET.xml&contenttype=SQLXML&platformno=15414").
+        to_return(:status => 200, :body => fixture("no_arrivals.xml"))
+
+      stub_request(:post, "https://foo:secret@api.smsified.com/v1/smsmessaging/outbound/555-555-1212/requests").
+         with(:body => {"address"=>"14075550100", "message"=>"No arrivals for next 30 minutes"}).
+         to_return(:status => 200, :body => "", :headers => {})
+
+      post :incoming, json
+      last_response.should be_ok
+    end
+
+    it "should return no arrivals for next 30 minutes" do
+      json = '{
+                "inboundSMSMessageNotification": {
+                  "inboundSMSMessage": {
+                    "dateTime": "2011-05-11T18:05:54.546Z",
+                    "destinationAddress": "16575550100",
+                    "message": "10246",
+                    "messageId": "ef795d3dac56a62fef3ff1852b0c123a",
+                    "senderAddress": "14075550100"
+                  }
+                }
+              }'
+
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?Name=RoutePositionET.xml&contenttype=SQLXML&platformno=10246").
+        to_return(:status => 200, :body => fixture("route_et.xml"))
+
+      stub_request(:post, "https://foo:secret@api.smsified.com/v1/smsmessaging/outbound/555-555-1212/requests").
+         with(:body => {"address"=>"14075550100", "message"=>"Route 1-Destination Castaic-ETA 24 minutes Route 4-Destination LARC-ETA 19 minutes Route 6-Destination Shadow Pines-ETA 17 minutes Route 14-Destination Plum Cyn-ETA 11 minutes "},
+              :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded'}).
+         to_return(:status => 200, :body => "", :headers => {})
+
+      post :incoming, json
+      last_response.should be_ok
+    end
+
+    it "should return no arrivals for next 30 minutes" do
+      json = '{
+                "inboundSMSMessageNotification": {
+                  "inboundSMSMessage": {
+                    "dateTime": "2011-05-11T18:05:54.546Z",
+                    "destinationAddress": "16575550100",
+                    "message": "10656",
+                    "messageId": "ef795d3dac56a62fef3ff1852b0c123a",
+                    "senderAddress": "14075550100"
+                  }
+                }
+              }'
+
+      stub_request(:get, "http://12.233.207.166/rtt/public/utility/file.aspx?Name=RoutePositionET.xml&contenttype=SQLXML&platformno=10656").
+        to_return(:status => 200, :body => fixture("one_arrival.xml"))
+
+      stub_request(:post, "https://foo:secret@api.smsified.com/v1/smsmessaging/outbound/555-555-1212/requests").
+         with(:body => {"address"=>"14075550100", "message"=>"Route 2 -Destination Val Verde -ETA 20 minutes"},
+              :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded'}).
+         to_return(:status => 200, :body => "", :headers => {})
+
+      post :incoming, json
+      last_response.should be_ok
+    end
+  end
 end
